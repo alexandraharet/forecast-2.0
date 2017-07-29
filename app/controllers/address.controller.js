81b8310f6b294eec21fd40cd2f26b9557aeca5d0
Alexandra Haret <alexandra.haret@gmail.com>
2017-07-29 10:47:34 +0100

    Refactored API calls using angular promises and serialized them in the address controller

--- app/controllers/address.controller.js
+++ app/controllers/address.controller.js
@@ -4,52 +4,35 @@
 	.module('weatherApp')
 	.controller('addressController', controller);
 
-	function controller($rootScope, $scope, $location, addressService, $q) {
+	function controller($rootScope, $scope, addressService, weatherService) {
 
-		var coordinates = undefined;
+		var coordinates;
 		var vm = this;
 
-		vm.title = 'Hello';
 		vm.address = "";
 		vm.weatherResult = undefined;
 		vm.formattedAddress = undefined;
 
-
-		// $scope.$watch("address", function () {
-		// 	addressService.address = $scope.address;
-		// });
-
 		vm.submit = function() {
-			vm.submitted = false;
-			setTimeout(function() {
-				vm.submitted = true;
-				addressService.callLocationApi(vm.address).then(
-					function(address) {
-						if(address.data.status === "ZERO_RESULTS") {
-							console.log("Could not find an address for '" + vm.address + "', please try again"); //TODO
-						}
-						else {
-							vm.formattedAddress = addressService.getFormattedAddress(address);
-							coordinates = addressService.getCoordinates(address);
-
-							addressService.callWeatherApi(coordinates.lat, coordinates.lon).then(
-								function(response) {
-
-									$q.defer =
-
-									console.log(coordinates);
-									console.log(response);
-								vm.weatherResult = response.data;
-							});
-
-						}
+			vm.submitted = true;
+			addressService.callLocationApi(vm.address)
+            .then(
+				function(addressResponse){
+					vm.formattedAddress = addressService.getFormattedAddress(addressResponse);
+					coordinates = addressService.getCoordinates(addressResponse);
+					return coordinates;
+					},
+				function(error) {
+					console.log(error);
+				})
+			.then(
+				function(coordinates) {
+					weatherService.callWeatherApi(coordinates.lat, coordinates.lon)
+					.then(
+						function(weatherResponse) {
+							vm.weatherResult = weatherResponse.data;
+						});
 				});
-
-			}, 300);
-		};
-
-		function formatAddress(data) {
-			return (data.results["0"].formatted_address);
-		}
+			};
 	}
 })();
--- app/services/services.js
+++ app/services/services.js
@@ -13,76 +13,41 @@ location API example: https://maps.googleapis.com/maps/api/geocode/json?address=
     .module('weatherApp')
     .factory('addressService', service);
 
-    function service($http, $resource) {
-
+    function service($http, $q) {
         var exports = {
             callLocationApi: callLocationApi,
             getCoordinates: getCoordinates,
-            getFormattedAddress: getFormattedAddress,
-            callWeatherApi: callWeatherApi
+            getFormattedAddress: getFormattedAddress
         };
 
-        var formattedAddress = "";
-        var coordinates = "";
-        var coordinates = undefined;
+        var formattedAddress, coordinates, addressApiResponse;
+
+        function getAddressFromApi(address) {
+            addressApiResponse = $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY');
+            return addressApiResponse;
+        }
 
         function callLocationApi(address) {
-            return $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY');
+            var deferred = $q.defer();
+            if(getAddressFromApi(address)) {
+                deferred.resolve(addressApiResponse);
+            }
+            else deferred.reject('Error returing the addressAPI response');
+            return deferred.promise;
         }
 
-        function getFormattedAddress(apiResponse) {
-            return apiResponse.data.results["0"].formatted_address;
+        function getFormattedAddress(addressApiResponse) {
+            formattedAddress = addressApiResponse.data.results["0"].formatted_address;
+            return formattedAddress;
         }
 
-        function getCoordinates(apiResponse) {
+        function getCoordinates(addressApiResponse) {
             coordinates = {
-                lat: apiResponse.data.results["0"].geometry.location.lat,
-                lon: apiResponse.data.results["0"].geometry.location.lng
+                lat: addressApiResponse.data.results["0"].geometry.location.lat,
+                lon: addressApiResponse.data.results["0"].geometry.location.lng
             };
             return coordinates;
         }
-
-        function callWeatherApi(lat, lon) {
-            return $http({
-                url: 'php/forecast.php',
-                method: 'get',
-                params: {
-                    lat: lat,
-                    lon: lon
-                    },
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            });
-        }
-        // function getWeather (lat, lon)
-
-        // return $q(function(resolve) {
-        //     var getCoordinates = $resource('https://maps.googleapis.com/maps/api/geocode/json:params', {get:{method:'JSONP'}});
-        //     var response = getCoordinates.get({
-        //         address: address,
-        //         key: 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY'
-        //     });
-        //     if(response) {
-        //         resolve(response);
-        //     }
-        // });
-
-        // var deferred = $q.defer();
-        // var getCoordinates = $resource('https://maps.googleapis.com/maps/api/geocode/json:params', {get:{method:'JSONP'}});
-        // var response = getCoordinates.get({
-        //     address: address,
-        //     key: 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY'
-        // });
-        //
-        // deferred.resolve(response);
-        //
-        // return deferred.promise;
-
-        // return getCoordinates.get({
-        //     address: address,
-        //     key: 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY'
-        // });
-        // }
-
         return exports;
     }
 })();
@@ -93,34 +58,36 @@ location API example: https://maps.googleapis.com/maps/api/geocode/json?address=
     .module('weatherApp')
     .factory('weatherService', service);
 
-    function service($resource, $q) {
+    function service($http, $q) {
 
-        var exports= {
-            getWeather: getWeather
+        var exports = {
+            callWeatherApi: callWeatherApi
         };
 
+        var weatherApiResponse;
 
-        function getWeather(lat, lon) {
+        function getWeatherFromApi(lat, lon) {
+            weatherApiResponse = $http({
+                url: 'php/forecast.php',
+                method: 'get',
+                params: {
+                    lat: lat,
+                    lon: lon
+                },
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+            return weatherApiResponse;
+        }
+
+        function callWeatherApi(lat, lon) {
             var deferred = $q.defer();
-            var weatherAPI = $resource('https://api.darksky.net/forecast/2cb2a6600011ce4ca629efa9e07cc9bd/:latlong', {jsonpCallbackParam: 'callback'}, {get:{method:'JSONP'}});
-
-            var response = function() {
-                weatherAPI.get({
-                    latlong: lat+","+lon,
-                    exclude: 'minutely,flags'
-                });
-            };
-
-            deferred.resolve(response);
-
+            if(getWeatherFromApi(lat, lon)) {
+                deferred.resolve(weatherApiResponse);
+            }
+            else deferred.reject('Error returing the weaherApi response');
             return deferred.promise;
-            // return weatherAPI.get({
-            //     latlong: lat+","+lon,
-            //     exclude: 'minutely,flags'
-            //     });
         }
 
         return exports;
     }
-
 })();
