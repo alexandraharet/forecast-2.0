cbaffc4789d237e9d8cacc8a17200080fe5540ff
Alexandra Haret <alexandra.haret@gmail.com>
2017-07-29 19:22:10 +0100

    Updated templates, refactored directives and started working with local scopes

--- app/directives/directives.js
+++ app/directives/directives.js
@@ -2,27 +2,47 @@
 
 angular
     .module('weatherApp')
-    .directive("weatherReportHourly", function() {
+    .directive("wfWeatherWrapper", function() {
     return {
         restrict: "E",
-        templateUrl: "templates/weatherReportHourly.html",
-        replace: true,
+        templateUrl: "templates/weatherWrapper.html",
         scope: {
-            weatherResult: "=",
             convertToStandard: "&",
             convertToDate: "&",
             convertToPercentage: "&",
+        },
+        controller: function($scope) {
+
         }
     };
 });
 
 angular
     .module('weatherApp')
-    .directive("weatherReportDaily", function() {
+    .directive("wfWeatherReportHourly", function() {
+    return {
+        restrict: "E",
+        templateUrl: "templates/weatherReportHourly.html",
+        scope: {
+            weatherResult: "="
+        },
+        controller: function($scope) {
+            var weatherResult = $scope.weatherResult;
+            $scope.$watch(weatherResult,
+                function() {
+                    console.log('watch triggered');
+                    console.log(weatherResult);
+                });
+        }
+    };
+});
+
+angular
+    .module('weatherApp')
+    .directive("wfWeatherReportDaily", function() {
     return {
         restrict: "E",
         templateUrl: "templates/weatherReportDaily.html",
-        replace: true,
         scope: {
             weatherResult: "=",
             convertToStandard: "&",
