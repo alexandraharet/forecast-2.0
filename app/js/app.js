dd76dadb3a277cb850038b6ae25fe8a9900faa55
Alexandra Haret <alexandra.haret@gmail.com>
2017-08-19 23:21:36 +0100

    Removed unused config option for module (lefotvers from v1 of the app)

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,12 @@
+//module
 (function(){
 	"use strict";
-
-	// module
 	angular
 		.module('weatherApp', ['ngRoute', 'ngResource', 'ngAnimate'])
 		.config(['$sceDelegateProvider', function ($sceDelegateProvider) {
-
 	    $sceDelegateProvider.resourceUrlWhitelist([
 	    // Allow same origin resource loads.
 	    'self',
-	    // Allow loading from our assets domain.  Notice the difference between * and **.
-	    'https://api.darksky.net/forecast/**',
-	    'https://maps.googleapis.com/maps/**'
 	  ]);
-
 	}]);
-
 })();
